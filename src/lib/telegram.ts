9af65f30bba345999522772a04a92b48bd88a361
jerry74 <jerry74@users.noreply.github.com>
2022-01-28 09:33:57 +0800

    Update telegram.ts

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -79,7 +79,7 @@ export async function useWebhook(req: NowRequest, res: NowResponse) {
 				params.append("text",content);
 
 				const ret = await axios.post( "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage" , params );
-				res.status(200).json( ret.data );
+				res.status(200).json('Ok');
 			}
 		}
 		
